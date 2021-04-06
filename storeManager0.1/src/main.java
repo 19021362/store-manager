c7c4766350d4e03fbd2e942846f4ac3b2988894d
19021362 <62606120+19021362@users.noreply.github.com>
2021-04-07 00:16:19 +0700

    Create main.java

---
+++ storeManager0.1/src/main.java
@@ -0,0 +1,23 @@
+import user.User;
+
+import javax.swing.text.PasswordView;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Manager manager  = new Manager();
+
+        manager.loadUser();
+        manager.Login();
+        if (manager.statusLog) {
+            manager.loadProduct();
+            manager.mainMenu();
+        }
+
+
+
+    }
+}
