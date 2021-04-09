3d9cd46bdaf2e41780f7761df23e0085166e6910
19021362 <62606120+19021362@users.noreply.github.com>
2021-04-10 01:04:30 +0700

    Update Bill_detail.java

--- storeManager0.1/src/bill/Bill_detail.java
+++ storeManager0.1/src/bill/Bill_detail.java
@@ -5,18 +5,21 @@ import product.*;
 public class Bill_detail {
     protected Product productInline;
     protected int quantity;
-    protected double sumInLine;
+    protected String id_bill;
+
 
     public Bill_detail() {
         this.productInline = null;
         this.quantity = 0;
-
     }
 
-    public Bill_detail(Product productInline, int quantity) {
+    public Bill_detail(String id_bill, Product productInline, int quantity) {
+        this.id_bill = id_bill;
         this.productInline = productInline;
         this.quantity = quantity;
     }
+
+
 
     public void setProductInline(Product productInline) {
         this.productInline = productInline;
@@ -24,6 +27,14 @@ public class Bill_detail {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public void setId(String id) {
+        this.id_bill = id;
+    }
+
+    public String getId_bill() {
+        return id_bill;
     }
 
     public int getQuantity() {
@@ -38,6 +49,10 @@ public class Bill_detail {
         return productInline.getPriceOut()*quantity*(1 - productInline.getDiscount());
     }
 
+    public double getProfitInLine() {
+        return getSumInLine() - (productInline.getPriceIn()*quantity);
+    }
+
     @Override
     public String toString() {
         return "| " + productInline.getProductName() +
@@ -45,5 +60,11 @@ public class Bill_detail {
                 "    | " + productInline.getPriceOut() +
                 "    | " + productInline.getDiscount() +
                 "    |= " + getSumInLine();
+    }
+
+    public String toSave() {
+        return id_bill + "," +
+                productInline.getProductName() + "," +
+                id_bill;
     }
 }
