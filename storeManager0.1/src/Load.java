a4ac139a93589f6b2fa6c20eac3b3cb2addea9cb
19021362 <62606120+19021362@users.noreply.github.com>
2021-04-10 01:04:37 +0700

    Update Load.java

--- storeManager0.1/src/Load.java
+++ storeManager0.1/src/Load.java
@@ -1,16 +1,24 @@
+import bill.Bill;
+import bill.Bill_detail;
 import product.Product;
 import user.User;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Load {
 
     private static String userFilePath = "file/user.txt";
     private static String productFilePath = "file/product.txt";
+    private static String billsFilePath = "file/bills.txt";
+    private static String billDetailFilePath = "file/billDetail.txt";
 
     public static ArrayList<User> loadUser() throws IOException {
 
@@ -44,6 +52,83 @@ public class Load {
             products.add(product);
         }
         return products;
+    }
+
+
+    public static Stack<Bill> loadBills(ArrayList<Bill_detail> allBillDetail) throws IOException {
+
+        Stack<Bill> bills = new Stack<>();
+        FileReader billFile = new FileReader(billsFilePath);
+        BufferedReader br = new BufferedReader(billFile);
+
+        String line = null;
+
+        while((line = br.readLine()) != null) {
+            String in[] = line.split("[,]");
+            Bill bill = new Bill(in[0], Time.valueOf(in[1]), Date.valueOf(in[2]), in[3], Long.parseLong(in[4]), in[5]);
+            bills.push(bill);
+        }
+
+        for (Bill b : bills) {
+            for (Bill_detail bd : allBillDetail) {
+                if (bd.getId_bill().equals(b.getIdBill())) {
+                    b.addBillDetail(bd);
+                }
+            }
+        }
+
+        return bills;
+    }
+
+
+    public static ArrayList<Bill_detail> loadBillDetail(ArrayList<Product> products) throws IOException {
+
+        ArrayList<Bill_detail> bill_details = new ArrayList<>();
+        FileReader bdtFile = new FileReader(billDetailFilePath);
+        BufferedReader br = new BufferedReader(bdtFile);
+
+        String line = null;
+
+        while((line = br.readLine()) != null) {
+            String in[] = line.split("[,]");
+            Product product = new Product();
+            for (Product p : products) {
+                if (p.getProductName().equals(in[1])) {
+                    product = p;
+                }
+            }
+            Bill_detail billDetail = new Bill_detail(in[0], product, Integer.parseInt(in[2]));
+            bill_details.add(billDetail);
+        }
+        return bill_details;
+    }
+
+
+
+    public static void saveData(ArrayList<User> users, ArrayList<Product> products, Stack<Bill> bills, ArrayList<Bill_detail> bill_details) throws IOException {
+        FileWriter fileUser = new FileWriter(userFilePath);
+        for (User u : users) {
+            fileUser.write(u.toSave() + "\n");
+        }
+        fileUser.close();
+
+        FileWriter fileProduct = new FileWriter(productFilePath);
+        for (Product p : products) {
+            fileProduct.write(p.toSave() + "\n");
+        }
+        fileProduct.close();
+
+        FileWriter fileBills = new FileWriter(billsFilePath);
+        for (Bill b : bills) {
+            fileBills.write(b.toSave() + "\n");
+        }
+        fileBills.close();
+
+        FileWriter fileBillDetail = new FileWriter(billDetailFilePath);
+        for (Bill_detail bd : bill_details) {
+            fileBillDetail.write(bd.toSave() + "\n");
+        }
+        fileBillDetail.close();
     }
 
 
