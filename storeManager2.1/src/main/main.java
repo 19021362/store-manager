780fb8d5fcc03f3829ee5a0f17d2a906b4c41b3d
19021362 <62606120+19021362@users.noreply.github.com>
2021-04-14 22:13:17 +0700

    Create main.java

---
+++ storeManager2.1/src/main/main.java
@@ -0,0 +1,32 @@
+package main;
+
+import main.Manager;
+import application.Login;
+import application.Login;
+import user.User;
+
+import javax.swing.text.PasswordView;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        Load.connectSQL();
+        Manager.loadUser();
+        
+        
+        Login login = new Login();
+        login.show();
+        
+        Manager.loadProduct();
+        Manager.loadBillDetail();
+        Manager.loadBills();
+
+
+
+
+
+
+    }
+}
